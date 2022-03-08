ae3165a25a1327c476ac7bf743338a30cfd0d9c5
Jacob Templeton <jwctempleton@gmail.com>
2022-03-08 13:00:09 -0800

    Add operate function that takes operator and two numbers and performs the specified operation on the two numbers

--- script.js
+++ script.js
@@ -14,3 +14,25 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      if (b === 0) {
+        return null;
+      } else {
+        return divide(a, b);
+      }
+    default:
+      return null;
+  }
+}
